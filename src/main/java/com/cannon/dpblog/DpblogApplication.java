738453d14f3f7d049979b0d704ea9bd3eb32603c
cannon <467366350@qq.com>
2019-11-12 17:04:08 +0800

    static files

---
+++ src/main/java/com/cannon/dpblog/DpblogApplication.java
@@ -0,0 +1,13 @@
+package com.cannon.dpblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpblogApplication.class, args);
+    }
+
+}
